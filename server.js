a8e6bc81a445b7c95f08ebf14664a8d713cf24a6
jisan Paramanik <sahanajpervin2006@gmail.com>
2026-07-01 03:11:45 +0600

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("🤖 GoodMan5.0 Server Running");
+});
+
+app.post("/chat", (req, res) => {
+  const message = req.body.message || "";
+  res.json({
+    reply: "আপনি লিখেছেন: " + message
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
+});
